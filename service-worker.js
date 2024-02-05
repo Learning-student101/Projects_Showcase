53723f404298be3b9ec108739740246a1583f445
Learning-student101 <150617584+Learning-student101@users.noreply.github.com>
2024-02-05 14:15:58 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,78 @@
+const CACHE_NAME = 'dynamic-cache-vTestA';
+const urlsToCache = [
+  'index.html',
+  'style.css',
+  'LoadingAnimation.js',
+  // other assets...
+];
+
+// Install event
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activate event
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          // Update the cache in the background
+          const fetchRequest = event.request.clone();
+          fetch(fetchRequest).then(
+            (newResponse) => {
+              if (newResponse && newResponse.status === 200 && newResponse.type === 'basic') {
+                const responseToCache = newResponse.clone();
+                caches.open(CACHE_NAME)
+                  .then((cache) => {
+                    cache.put(event.request, responseToCache);
+                  });
+              }
+            }
+          );
+          return response; // Return the cached response immediately
+        }
+
+        // No cache hit, fetch from network
+        return fetch(event.request).then(
+          (response) => {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+  );
+});
+
+
